4fc417515d2713d94eec913e3387e8319d36a102
Jiaxuan Chen <jiaxuan.chen2@mail.mcgill.ca>
2026-03-06 10:25:42 -0500

    update readme

--- S-LoRA/benchmarks/paper/ablation_mem/launch_server_a10g_S1_dm.sh
+++
@@ -1,1 +0,0 @@
-python ../launch_server.py  --device a10g  --model-setting S1  --backend slora  --num-adapter 200  --num-token 14500
--- S-LoRA/benchmarks/paper/ablation_mem/launch_server_a10g_S1_no_mem.sh
+++
@@ -1,1 +0,0 @@
-python ../launch_server.py  --device a10g  --model-setting S1  --backend slora  --num-adapter 200  --num-token 15000 --no-mem-pool
